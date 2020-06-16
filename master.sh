801a47da15b2b0b8b7d3dd856aba199a909f1e4c
Oliver Cairns <o.cairns@marketfinance.com>
2020-06-16 11:37:02 +0100

    update

--- master.sh
+++ master.sh
@@ -2,6 +2,8 @@
 
 /opt/anaconda3/bin/python3 rumour_scraper.py
 
+export PATH=$PATH:/opt/anaconda3/bin:/opt/anaconda3/condabin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin: No such file or directory
+
 git add .
 
 git commit -m 'update'
