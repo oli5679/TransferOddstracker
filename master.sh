5ef7b2ed0bbd8786cad2e64eaa570b01082e75b5
Oliver Cairns <o.cairns@marketfinance.com>
2020-06-16 11:48:01 +0100

    update

--- master.sh
+++ master.sh
@@ -1,9 +1,11 @@
-/opt/anaconda3/bin/conda activate base
+#!/usr/bin/env bash
+#/opt/anaconda3/bin/conda activate base
 
-export PATH=$PATH:/opt/anaconda3/bin:/opt/anaconda3/condabin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin: No such file or directory
+#export PATH=$PATH:/opt/anaconda3/bin:/opt/anaconda3/condabin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin: No such file or directory
 
 /opt/anaconda3/bin/python3 rumour_scraper.py
 
+python3 rumour_scraper.py
 sudo git config --system user.email "olicairns93@gmail.com"
 sudo git config --system user.name "oli5679"
 
