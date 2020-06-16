6aa18394a5f120b2404ff9855db5aae7c0fa0c51
Oliver Cairns <o.cairns@marketfinance.com>
2020-06-16 11:39:25 +0100

    update

--- master.sh
+++ master.sh
@@ -1,8 +1,8 @@
 /opt/anaconda3/bin/conda activate base
 
-/opt/anaconda3/bin/python3 rumour_scraper.py
-
 export PATH=$PATH:/opt/anaconda3/bin:/opt/anaconda3/condabin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin: No such file or directory
+
+/opt/anaconda3/bin/python3 rumour_scraper.py
 
 git add .
 
